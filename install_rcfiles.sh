7bb161157a6ff1158f918c57836d8ebf3a1fe33f
Adrian Siembida <bida94@gmail.com>
2020-04-10 17:59:29 +0200

    extend install file

--- install_rcfiles.sh
+++ install_rcfiles.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 
-rm ~/.config/nvim/init.vim
-rm ~/.bash_aliases
-rm ~/.tmux.conf
+rm ~/.config/nvim/init.vim ~/.bash_aliases ~/.tmux.conf
 
 ln -s ~/rcfile/init.vim ~/.config/nvim/init.vim
 ln -s ~/rcfile/.bash_aliases ~/.bash_aliases
 ln -s ~/rcfile/.tmux.conf ~/.tmux.conf
+
+source ~/.bashrc
+
+nvim +PlugInstall +qall
